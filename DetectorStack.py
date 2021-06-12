b6c780077a69d5151a40aab65b091880d2f24e74
william <sette095@umn.edu>
2021-06-12 18:54:29 -0500

    fix effective area. make energy dispersion optional for response matrix.

--- DetectorStack.py
+++ DetectorStack.py
@@ -11,13 +11,14 @@ class DetectorStack:
         self.materials = materials
         self.photon_detector = photon_detector
 
-    def generate_detector_response_to(self, incident_spectrum: FlareSpectrum) -> np.ndarray:
+    def generate_detector_response_to(self, incident_spectrum: FlareSpectrum, disperse_energy: bool=True) -> np.ndarray:
         response = self.generate_attenuation_response_due_to(incident_spectrum)
-        return apply_detector_dispersion_for(incident_spectrum, response)
+        return self._dispatch_dispersion(incident_spectrum, response, disperse_energy)
 
     def generate_attenuation_response_due_to(self, incident_spectrum) -> np.ndarray:
         response = np.identity(incident_spectrum.energies.shape[0])
-        for material in self.materials[:-1]:
+        # BUG WAS HERE :(
+        for material in self.materials:
             response = np.matmul(material.generate_overall_response_matrix_given(incident_spectrum), response)
         return response
 
@@ -25,10 +26,14 @@ class DetectorStack:
         pd_spread = self.photon_detector.generate_energy_resolution_given(incident_spectrum)
         return np.matmul(pd_spread, resp_matrix)
 
+    def _dispatch_dispersion(self, incident_spectrum: FlareSpectrum, response: np.ndarray, do_it: bool):
+        if do_it: return self.apply_detector_dispersion_for(incident_spectrum, response)
+        else: return response
+
     def generate_effective_area_due_to(self, incident_spectrum) -> np.ndarray:
         # area vector
         av = np.ones(incident_spectrum.energies.size) * self.area
-        return np.matmul(self.generate_detector_response_to(incident_spectrum), av)
+        return np.matmul(self.generate_detector_response_to(incident_spectrum, disperse_energy=False), av)
 
     def respond_to(self, incident_spectrum) -> np.ndarray:
         self.generate_detector_response_to(incident_spectrum) * incident_spectrum.energies
@@ -45,10 +50,12 @@ class HafxStack(DetectorStack):
         # take off the scintillator to treat it separately
         self.scintillator = self.materials.pop()
 
-    def generate_detector_response_to(self, incident_spectrum: FlareSpectrum) -> np.ndarray:
+    def generate_detector_response_to(
+            self, incident_spectrum: FlareSpectrum, disperse_energy: bool=True) -> np.ndarray:
         response = self.generate_attenuation_response_due_to(incident_spectrum)
         # now incorporate the scintillator
         ident = np.identity(incident_spectrum.energies.shape[0])
         absorbed = ident - self.scintillator.generate_overall_response_matrix_given(incident_spectrum)
         response = np.matmul(absorbed, response)
-        return self.apply_detector_dispersion_for(incident_spectrum, response)
+        return self._dispatch_dispersion(incident_spectrum, response, disperse_energy)
+
