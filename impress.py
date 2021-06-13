1c5b6c4db39250e0b122ad0f5e56f186034e7cda
william <sette095@umn.edu>
2021-06-12 19:03:26 -0500

    shift around impress sim v1

--- impress.py
+++ impress.py
@@ -8,23 +8,32 @@ from Instrument import Instrument
 from Material import Material
 from PhotonDetector import Sipm3000
 
-def stack_with_thickness(al_thick):
-    material_order = [ic.AL, ic.TE, ic.BE, ic.CEBR3]
+def base_stack():
+    material_order = [ic.TE, ic.BE, ic.CEBR3]
     materials = []
     for mat_name in material_order:
-        # use passed-in thickness if it's aluminum
-        thick = ic.THICKNESSES[mat_name] if mat_name != ic.AL else al_thick   #cm
+        thick = ic.THICKNESSES[mat_name]                                      # cm
         rho = ic.DENSITIES[mat_name]                                          # g / cm3
         atten_dat = AttenuationData.from_nist_file(ic.ATTEN_FILES[mat_name])
         materials.append(Material(ic.DIAMETER, thick, rho, atten_dat))
-    return HafxStack(materials, Sipm3000())
+
+    return materials
+
 
 def build_impress(aluminum_thicknesses):
     # order is: Al, Teflon, Be, CeBr3
     detector_stacks = []
-    for this_thickness in aluminum_thicknesses:
-        detector_stacks.append(stack_with_thickness(this_thickness))
+    base_mat_stack = base_stack()
+    for al_thick in aluminum_thicknesses:
+        ad = AttenuationData.from_nist_file(ic.ATTEN_FILES[ic.AL])
+        alum = Material(
+                ic.DIAMETER, al_thick, ic.DENSITIES[ic.AL], ad)
+        # all DetectorStacks reference the same base material objects
+        # therefore, their saved matrix hashes are shared.
+        cur_stack = [alum] + base_mat_stack
+        detector_stacks.append(HafxStack(cur_stack, Sipm3000()))
     return Instrument(detector_stacks)
+
 
 def impress_flare_spectrum(goes_flux, e_start, e_end, de):
     # save a hashmap of flares so that it doesn't recompute every time (slow)
@@ -38,28 +47,29 @@ def impress_flare_spectrum(goes_flux, e_start, e_end, de):
             impress_flare_spectrum.hash = { goes_flux : spec }
         return spec
 
+
 # TODO: add a goes_flux variable to FlareSpectrum to use for hashing elsewhere
 def test():
     goes_class = 'M1'
     goes_flux = goes_class_lookup(goes_class)       # W / cm2
     al_thicks = np.array([10, 20, 120, 190]) * 1e-4 # cm
     e_start = 1                                     # keV
-    e_end = 300                                     # keV
-    de = 0.1                                        # keV
+    e_end = 100                                     # keV
+    de = 0.05                                       # keV
 
     impress = build_impress(al_thicks)
     flare_spectrum = impress_flare_spectrum(goes_flux, e_start, e_end, de)
     count_vectors = []
-    eff_area_vecs = []
+    # eff_area_vecs = []
     for det in impress.detector_stacks:
         response = det.generate_detector_response_to(flare_spectrum)
         count_vectors.append(np.matmul(response, flare_spectrum.flare))
-        base_area = np.ones(flare_spectrum.energies.size) * det.area
-        eff_area_vecs.append(np.matmul(response, base_area))
+        # base_area = np.ones(flare_spectrum.energies.size) * det.area
+        # eff_area_vecs.append(np.matmul(response, base_area))
 
-    outf = 'area_out.tab'
-    # out = [flare_spectrum.energies] + [e for e in count_vectors] + [flare_spectrum.flare]
-    out = [flare_spectrum.energies] + [a for a in eff_area_vecs]
+    outf = 'flare_out.tab'
+    out = [flare_spectrum.energies] + [e for e in count_vectors] + [flare_spectrum.flare]
+    # out = [flare_spectrum.energies] + [a for a in eff_area_vecs]
     np.savetxt(outf, np.transpose(out))
 #     total = np.trapz(count_vectors, x=flare_spectrum.energies, dx=de, axis=1) * impress.detector_stacks[0].area
 #     print(f"Total counts: {total}")
--- impress_constants.py
+++ impress_constants.py
@@ -48,3 +48,5 @@ DENSITIES = {
 
 FULL_AREA = 43                                  # cm2 
 DIAMETER = 2 * np.sqrt(FULL_AREA / 4 / np.pi)   # cm
+
+RESPONSE_MAT_DIR = 'resp-mat'
