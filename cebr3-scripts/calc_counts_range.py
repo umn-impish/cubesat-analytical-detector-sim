1722989e029b1992fe8169ca9d66ea6a9edc026b
William Setterberg <william.s.setterberg@gmail.com>
2022-08-30 12:24:35 -0500

    modify calc_counts_range script to print in a prettier way and provide more information for Julie's instrument proposal questions

--- cebr3-scripts/calc_counts_range.py
+++ cebr3-scripts/calc_counts_range.py
@@ -8,38 +8,52 @@ CEBR3_AREA = 43/4
 opt_dir = f'{os.path.dirname(__file__)}/../responses-and-areas/optimized-2022-aug-22-bins'
 filez = os.listdir(opt_dir)
 
-base = "{:<30} " * 4
+portion_sizes = [13, 33, 18, 38]
+base = '| '.join(f'{{:<{ps}}}' for ps in portion_sizes)
+which_fmt = '{} flare incident on {} detector'
 
-print(base.format("Which", "Flare size", "Al thickness (um)", f"count/cm2/sec for energy in ({ENG_LOWER_BOUND}, {ENG_UPPER_BOUND}) keV"))
+print('load containers')
 att_cps_cm2_d = dict()
 orig_cps_cm2_d = dict()
 thickness_dict = dict()
-for f in filez:
-    cur_con = HafxSimulationContainer.from_saved_file(os.path.join(opt_dir, f))
-    fl = cur_con.flare_spectrum.flare
-    en = cur_con.flare_spectrum.energy_edges
-    att_fl = np.matmul(
-        cur_con.matrices[cur_con.KDISPERSED_RESPONSE],
-        fl)
+containers = [HafxSimulationContainer.from_file(os.path.join(opt_dir, f)) for f in filez]
+containers.sort(key=lambda c: c.goes_class)
+print('done loading')
 
-    restrict = np.logical_and(en >= ENG_LOWER_BOUND, en <= ENG_UPPER_BOUND)
+print('calculating')
+for flare_con in containers:
+    for cur_con in containers:
+        fl = flare_con.flare_spectrum.flare
+        en = cur_con.flare_spectrum.energy_edges
+        att_fl = cur_con.matrices[cur_con.KDISPERSED_RESPONSE] @ fl
 
-    # value * bin width
-    att_counts_cm2_sec = np.sum((att_fl * np.diff(en))[restrict[:-1]])
-    orig_counts_cm2_sec = np.sum((fl * np.diff(en))[restrict[:-1]])
+        restrict = np.logical_and(en >= ENG_LOWER_BOUND, en <= ENG_UPPER_BOUND)
 
-    fl_sz = cur_con.flare_spectrum.goes_class
-    att_cps_cm2_d[fl_sz] = att_counts_cm2_sec
-    orig_cps_cm2_d[fl_sz] = orig_counts_cm2_sec
-    thickness_dict[fl_sz] = cur_con.al_thick * 1e4
+        # value * bin width
+        att_counts_cm2_sec = np.sum((att_fl * np.diff(en))[restrict[:-1]])
+        orig_counts_cm2_sec = np.sum((fl * np.diff(en))[restrict[:-1]])
 
-line_fmt = "{:<30} {:<30} {:<30.2f} {:<30.0f}"
-for k in sorted(att_cps_cm2_d.keys(), key=lambda x: ord(x[0]) + int(x[1])):
-    print(line_fmt.format('original', k, thickness_dict[k], orig_cps_cm2_d[k]))
+        fl_sz = flare_con.goes_class
+        att_id = cur_con.goes_class
+        ident = which_fmt.format(fl_sz, att_id)
+        att_cps_cm2_d[ident] = att_counts_cm2_sec
+        orig_cps_cm2_d[ident] = orig_counts_cm2_sec
+        thickness_dict[ident] = cur_con.al_thick * 1e4
+print('done calculating')
+
+line_fmt = "{{:<{:d}}}| {{:<{:d}}}| {{:<{:d}.2f}}| {{:<{:d}.0f}}".format(*portion_sizes)
+sep_string = '=' * ((len(portion_sizes) - 1)*2 + sum(portion_sizes))
+print(base.format("Which", "Flare size", "Al thickness (um)", f"count/cm2/sec for energy in ({ENG_LOWER_BOUND}, {ENG_UPPER_BOUND}) keV"))
+print(sep_string)
+for i, k in enumerate(sorted(att_cps_cm2_d.keys(), key=lambda x: ord(x[0]) + int(x[1]))):
+    # print(line_fmt.format('original', k, thickness_dict[k], orig_cps_cm2_d[k]))
     print(line_fmt.format('attenuated', k, thickness_dict[k], att_cps_cm2_d[k]))
+    if (i + 1) % 4 == 0: print(sep_string)
 
 print()
 print(base.format("Which", "Flare size", "Al thickness (um)", f"count/sec for energy in ({ENG_LOWER_BOUND}, {ENG_UPPER_BOUND}) keV"))
-for k in sorted(att_cps_cm2_d.keys(), key=lambda x: ord(x[0]) + int(x[1])):
-    print(line_fmt.format('original', k, thickness_dict[k], orig_cps_cm2_d[k] * CEBR3_AREA))
+print(sep_string)
+for i, k in enumerate(sorted(att_cps_cm2_d.keys(), key=lambda x: ord(x[0]) + int(x[1]))):
+    # print(line_fmt.format('original', k, thickness_dict[k], orig_cps_cm2_d[k] * CEBR3_AREA))
     print(line_fmt.format('attenuated', k, thickness_dict[k], att_cps_cm2_d[k] * CEBR3_AREA))
+    if (i + 1) % 4 == 0: print(sep_string)
